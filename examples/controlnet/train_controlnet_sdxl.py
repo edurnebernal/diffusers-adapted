5ccfd9cf4a4628ceac02ea39f1658cd264694195
edurnebernal <edurnebernal@unizar.es>
2025-05-07 16:12:36 +0200

    fix preprocess_train to load images

--- examples/controlnet/train_controlnet_sdxl.py
+++ examples/controlnet/train_controlnet_sdxl.py
@@ -750,10 +750,10 @@ def prepare_train_dataset(dataset, accelerator):
     )
 
     def preprocess_train(examples):
-        images = [image.convert("RGB") for image in examples[args.image_column]]
+        images = [Image.open(image).convert("RGB") for image in examples[args.image_column]]
         images = [image_transforms(image) for image in images]
 
-        conditioning_images = [image.convert("RGB") for image in examples[args.conditioning_image_column]]
+        conditioning_images = [Image.open(image).convert("RGB") for image in examples[args.conditioning_image_column]]
         conditioning_images = [conditioning_image_transforms(image) for image in conditioning_images]
 
         examples["pixel_values"] = images
